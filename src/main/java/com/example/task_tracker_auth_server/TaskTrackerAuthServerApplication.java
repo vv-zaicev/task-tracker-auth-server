12f3bb047b2ef4b5ea62fb4cf23dd9375b0d4f04
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-07-27 22:03:26 +0600

    инициализирующий коммит

---
+++ src/main/java/com/example/task_tracker_auth_server/TaskTrackerAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_tracker_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task_tracker_auth_server/TaskTrackerAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_tracker_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
